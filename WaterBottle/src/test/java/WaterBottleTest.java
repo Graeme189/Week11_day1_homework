ad992e2422d0ab762d63d66cdf831347e2978b4a
Graeme Henderson <graeme.189@gmail.com>
2019-09-02 15:59:40 +0100

    Water Bottle homework complete

---
+++ WaterBottle/src/test/java/WaterBottleTest.java
@@ -0,0 +1,29 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
+public class WaterBottleTest {
+
+    WaterBottle waterbottle;
+
+    @Before
+    public void before() {
+        waterbottle = new WaterBottle(100);
+    }
+
+    @Test
+    public void drink() {
+        assertEquals(90, waterbottle.drink());
+    }
+
+    @Test
+    public void empty() {
+        assertEquals(0, waterbottle.empty());
+    }
+
+    @Test
+    public void fill() {
+        assertEquals(100, waterbottle.fill());
+    }
+}
